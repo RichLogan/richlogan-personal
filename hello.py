8de03498628868aaa98073608a7d7a16b6a6c7fc
Rich Logan <rilogan@cisco.com>
2014-02-07 11:17:57 -0800

    Inital

--- hello.py
+++ hello.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
-app.debug = True
 
 @app.route('/')
 def index():
 	return render_template('index.html')
 
+if __name__ == "__main__":
+	app.run(debug=True)
