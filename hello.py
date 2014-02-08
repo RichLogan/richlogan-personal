5aa3ca2d43bb957aa6ad91e5f8225ca8531a9a1c
Rich Logan <rilogan@cisco.com>
2014-02-07 18:45:22 -0800

    revert

--- hello.py
+++ hello.py
@@ -7,11 +7,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-	london = timezone('Europe/London')
-	londonTime = datetime.now(london)
-	sanJose = timezone('America/Los_Angeles')
-	sanJoseTime = datetime.now(sanJose)
-	return render_template('index.html', londonTime = londonTime.strftime('%d/%m %H:%M'), sanJoseTime = sanJoseTime.strftime('%A %d/%m %H:%M'))
+	#london = timezone('Europe/London')
+	#londonTime = datetime.now(london)
+	#sanJose = timezone('America/Los_Angeles')
+	#sanJoseTime = datetime.now(sanJose)
+	return render_template('index.html')
+	#, londonTime = londonTime.strftime('%d/%m %H:%M'), sanJoseTime = sanJoseTime.strftime('%A %d/%m %H:%M'))
 
 if __name__ == "__main__":
 	app.run(debug=True)
