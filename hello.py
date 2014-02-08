9c358f90c6741635464ee1d389ba77ce55137191
Rich Logan <rilogan@cisco.com>
2014-02-07 18:38:26 -0800

    Eric

--- hello.py
+++ hello.py
@@ -1,10 +1,17 @@
 import os
 from flask import Flask, render_template
+from datetime import datetime
+from pytz import timezone
+import pytz
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	london = timezone('Europe/London')
+	londonTime = datetime.now(london)
+	sanJose = timezone('America/Los_Angeles')
+	sanJoseTime = datetime.now(sanJose)
+	return render_template('index.html', londonTime = londonTime.strftime('%d/%m %H:%M'), sanJoseTime = sanJoseTime.strftime('%d/%m %H:%M'))
 
 if __name__ == "__main__":
 	app.run(debug=True)
